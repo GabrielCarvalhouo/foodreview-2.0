177c504ef879978247309b3838ed9d9406a5c493
Gabriel Vasconcellos <gabrielvasconcellos@pop-os.localdomain>
2022-09-29 21:32:19 -0300

    Update-09-29-2022

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -18,7 +18,7 @@ const CustomInput = ({ value, setValue, placeholder, secureTextEntry }) => {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'lightgray',
-        wigth: '100%',
+        width: '100%',
         marginVertical: 5,
         borderWidth: 1,
         borderColor: '#e8e8e8',
